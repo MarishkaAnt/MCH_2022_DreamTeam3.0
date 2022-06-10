9d4679696e63cedb6e7243fe5dafe8a5b21c06c0
Aleksandr Sokolov <aleksandr_sokolov1@epam.com>
2022-06-11 01:38:01 +0300

    изменил проперти

---
+++ backend/rest/src/main/java/com/dreamteam3/rest/model/UserDTO.java
@@ -0,0 +1,14 @@
+//package com.dreamteam3.rest.model;
+//
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class UserDTO {
+//private Long id;
+//
+//}
