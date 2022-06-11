d16e8978f2ae3e3aedb3f7069d1d909db2d6d4cd
Egorova <a.egorova@indevstudio.com>
2022-06-11 23:00:49 +0300

    add default constructors to dto

--- backend/data/src/main/java/com/dreamteam3/data/dto/RoleDto.java
+++ backend/data/src/main/java/com/dreamteam3/data/dto/RoleDto.java
@@ -3,10 +3,12 @@ package com.dreamteam3.data.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class RoleDto {
 
     private Long id;
