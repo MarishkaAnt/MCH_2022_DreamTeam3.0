8963a55ff4f33df7eb3a5ca456c8351e6c60eddb
Egorova <a.egorova@indevstudio.com>
2022-06-11 10:27:36 +0300

    add dto

---
+++ backend/data/src/main/java/com/dreamteam3/data/dto/CompanyDto.java
@@ -0,0 +1,17 @@
+package com.dreamteam3.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class CompanyDto {
+
+    private Long id;
+    private String name;
+    private String inn;
+    private String address;
+
+}
