360b147e3c83e608fe4292af702a44faedd3a28c
Mariia Antonova <mariia_antonova@epam.com>
2022-06-12 16:22:07 +0300

    add validation

--- backend/data/src/main/java/com/dreamteam3/data/model/User.java
+++ backend/data/src/main/java/com/dreamteam3/data/model/User.java
@@ -6,6 +6,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Entity
 @Table(name = "users")
@@ -14,6 +17,8 @@ import javax.persistence.*;
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+
+    public static final String PASSWORD_FORMAT = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$";
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -24,10 +29,16 @@ public class User {
     @OneToOne
     @JoinColumn(name = "company_id")
     private Company company;
+    @NotNull(message = "First name must be filled in")
     private String firstName;
+    @NotNull(message = "Last name must be filled in")
     private String lastName;
     private String patronymic;
+    @Email
     private String email;
+    @NotNull(message = "Password must be minimum 8 characters, and contain at least one uppercase letter, " +
+            "one lowercase letter and one number")
+    @Pattern(regexp = PASSWORD_FORMAT)
     private String password;
 
 }
