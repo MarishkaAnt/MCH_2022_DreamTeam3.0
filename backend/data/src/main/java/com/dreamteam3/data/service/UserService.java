f0d615ea921a47d114476d8788c0acb2408720e2
Egorova <a.egorova@indevstudio.com>
2022-06-13 00:16:35 +0300

    add login controller

--- backend/data/src/main/java/com/dreamteam3/data/service/UserService.java
+++ backend/data/src/main/java/com/dreamteam3/data/service/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +20,10 @@ public class UserService {
 
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmailIgnoreCase(email);
     }
 
     public User save(User user) {
