f33f3f6ddb0e0d4ab727a6bcacc15a160bc0ea45
Mariia Antonova <mariia_antonova@epam.com>
2022-06-12 19:04:50 +0300

    fixed permissions

--- backend/rest/src/main/java/com/dreamteam3/rest/config/SecurityConfig.java
+++ backend/rest/src/main/java/com/dreamteam3/rest/config/SecurityConfig.java
@@ -4,13 +4,13 @@ import com.dreamteam3.rest.auth.CustomAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -32,10 +32,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter =
+                new CustomAuthenticationFilter(authenticationManagerBean());
+        customAuthenticationFilter.setFilterProcessesUrl("/api/v1/login");
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeRequests().anyRequest().permitAll();
-        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+        http.authorizeRequests().antMatchers("/swagger-ui/**",
+                "/swagger-ui.html", "/api/v1/login/**").permitAll();
+        http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/v1/user").permitAll();
+        http.authorizeRequests().antMatchers("/api/v1/**").authenticated()
+                .and()
+                .formLogin()
+                .defaultSuccessUrl("/api/v1", true)
+                .permitAll()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable()
+                .logout()
+                .logoutSuccessUrl("/api/v1/user");
+
+        //http.authorizeRequests().antMatchers("/api/v1/**").authenticated();
+        http.addFilter(customAuthenticationFilter);
     }
 
     @Bean
