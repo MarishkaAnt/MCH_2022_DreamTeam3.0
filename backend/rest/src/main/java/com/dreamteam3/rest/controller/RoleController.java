4bf9e83783d3ff8d07b7d47976676bfa2d639520
Egorova <a.egorova@indevstudio.com>
2022-06-13 00:19:02 +0300

    fix cross origin

--- backend/rest/src/main/java/com/dreamteam3/rest/controller/RoleController.java
+++ backend/rest/src/main/java/com/dreamteam3/rest/controller/RoleController.java
@@ -3,6 +3,7 @@ package com.dreamteam3.rest.controller;
 import com.dreamteam3.data.model.Role;
 import com.dreamteam3.data.service.RoleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/role")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class RoleController {
 
     private final RoleService roleService;
